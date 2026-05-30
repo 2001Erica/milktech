70b24febdb7dffe2275e3a7cabd4c5ee1c78b0f9
unknown <nationalgold2022@gmail.com>
2026-05-30 16:34:59 -0300

    adiciona pagina inicial

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/',(req,res)=>{
+  res.sendFile(_dirname + '/index02.html');
+});
+
 const db = new sqlite3.Database('./milktech.db');
 
 /* CRIAR TABELA */
